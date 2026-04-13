2d9e4c8840252adf4ca49f2596739161f88464f9
Andreas Højrup <andreas.hovaldt@gmail.com>
2026-04-13 16:05:10 +0200

    Modified the 3detr-utonia config for training and added slurm script in the submodule

--- configs/scannet/det-3detr-utonia-v1m1-0-scannet.py
+++ configs/scannet/det-3detr-utonia-v1m1-0-scannet.py
@@ -40,7 +40,7 @@ _base_ = ["../_base_/default_runtime.py"]
 
 # ── Training ─────────────────────────────────────────────────────────────────
 batch_size = 8       # total across all GPUs
-num_worker = 16
+num_worker = 32
 mix_prob = 0         # detection dataset does not support MixUp
 enable_amp = False
 find_unused_parameters = False
