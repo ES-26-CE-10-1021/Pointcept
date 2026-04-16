ef11d752069dad292c6d6eb2e910eb223939d2de
Andreas Hovaldt Hojrup <andreas.hovaldt@gmail.com>
2026-04-16 12:05:14 +0200

    Fixed the environment setup

--- setup_pointcept_env.sh
+++ setup_pointcept_env.sh
@@ -2,8 +2,14 @@
 set -eo pipefail
 
 ENV_NAME="pointcept"
+
+echo "==> Initializing Git submodules..."
+git submodule update --init --recursive
+
 unset PIP_CONSTRAINT # Disregard /etc/pip/constraint.txt
 
+# Define cuda compute capability
+echo "==> Getting GPU Cuda compute capability..."
 if command -v nvidia-smi &> /dev/null && nvidia-smi -i 0 &> /dev/null; then
     export TORCH_CUDA_ARCH_LIST="$(nvidia-smi -i 0 --query-gpu=compute_cap --format=csv,noheader)"
     # https://developer.nvidia.com/cuda-gpus
@@ -12,8 +18,9 @@ else
     export TORCH_CUDA_ARCH_LIST="8.0"
 fi
 
+# Create conda environment
+echo "==> Creating conda env..."
 conda env create --file=environment.yml
-
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "${ENV_NAME}"
 
@@ -21,9 +28,20 @@ conda activate "${ENV_NAME}"
 export CUDA_HOME="${CONDA_PREFIX}"
 export PATH="${CONDA_PREFIX}/bin:${PATH}"
 
+# Pip installs
+echo "==> Installing additional pip dependencies..."
+export CPATH=$CONDA_PREFIX/targets/x86_64-linux/include:$CPATH
+
+# Pre-compiled wheels
+pip install torch-cluster torch-scatter torch-sparse -f https://data.pyg.org/whl/torch-2.5.0+cu124.html
+pip install torch_geometric spconv-cu124 peft
+
+# Source builds requiring PyTorch (MUST use --no-build-isolation)
+pip install --no-build-isolation git+https://github.com/octree-nn/ocnn-pytorch.git
+pip install --no-build-isolation git+https://github.com/openai/CLIP.git
+MAX_JOBS=8 pip install --no-build-isolation --no-cache-dir flash-attn==2.7.4.post1
 pip install --no-build-isolation ./libs/pointops
 pip install --no-build-isolation ./libs/pointgroup_ops
-MAX_JOBS=8 pip install --no-build-isolation --no-cache-dir flash-attn==2.7.4.post1
 
 # 3DETR dependencies
 echo "==> Building 3DETR dependencies"
@@ -40,4 +58,3 @@ pip install --no-build-isolation ./third_party/LitePT/libs/pointrope
 pip install pytest
 
 echo "==> Done! Activate with: conda activate ${ENV_NAME}"
-
