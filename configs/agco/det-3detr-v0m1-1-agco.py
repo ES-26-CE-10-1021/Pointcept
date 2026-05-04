90618aebbaa0504c1991c00e0dfb4d20d5cbfb37
Andreas Hovaldt Hojrup <andreas.hovaldt@gmail.com>
2026-05-04 15:27:31 +0200

    Debugging

--- configs/agco/det-3detr-v0m1-1-agco.py
+++ configs/agco/det-3detr-v0m1-1-agco.py
@@ -137,7 +137,7 @@ data = dict(
         # residual_rpy_warn_deg=5,
         
         transform=[
-            dict(type="RandomFlipDetection", p_x=0.0, p_y=0.5),
+            dict(type="RandomFlipDetection", p_x=0.5, p_y=0.5),
             dict(type="RandomRotateZDetection", angle_deg=(-5.0, 5.0)),
             # dict(type="RandomScaleDetection", scale=(0.9, 1.1), apply_to_sizes=True),
             # dict(type="RandomJitterDetection", sigma=0.005, clip=0.02),
@@ -151,7 +151,7 @@ data = dict(
         root_dir=data_root,
         meta_data_dir=meta_data_dir,
         
-        split="val",
+        split="train",
         split_prefix="agco",
         
         sensors=sensors,
@@ -179,7 +179,7 @@ data = dict(
         root_dir=data_root,
         meta_data_dir=meta_data_dir,
 
-        split="test",
+        split="train",
         split_prefix="agco",
 
         sensors=sensors,
