73447668bec72045f42402f679e4afef0da9f033
Andreas Højrup <andreas.hovaldt@gmail.com>
2026-05-15 15:25:56 +0200

    Fixed configs to have default values aligning with running on agco-cluster

--- configs/scannet/det-3detr-utonia-v5m1-1-scannet-nocolor.py
+++ configs/scannet/det-3detr-utonia-v5m1-1-scannet-nocolor.py
@@ -18,7 +18,7 @@ _base_ = ["../_base_/default_runtime.py"]
 
 # ── Training ─────────────────────────────────────────────────────────────────
 batch_size = 8
-num_worker = 64
+num_worker = 16
 mix_prob = 0
 enable_amp = False
 find_unused_parameters = True
